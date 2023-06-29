d6dddd14b3c265811c9bcdd8f7b60029d22ae1c1
Nish-patel16 <pate1088@algonquinlive.com>
2023-06-28 23:26:16 -0400

    week 4 completed

--- app/src/main/java/algonquin/cst2335/pate1088/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/pate1088/MainActivity.java
@@ -2,7 +2,14 @@ package algonquin.cst2335.pate1088;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        Button loginButton = findViewById(R.id.button);
+        EditText EmailAddress = findViewById(R.id.emailanswer);
+
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        String emailAddress = prefs.getString("LoginName", "");
+
+        Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+
+        EmailAddress.setText(emailAddress);
+
+        loginButton.setOnClickListener(clk->{
+            Intent: nextPage.putExtra("EmailAddress", EmailAddress.getText().toString());
+            editor.putString("LoginName", EmailAddress.getText().toString());
+            editor.apply();
+            startActivity(nextPage);
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "The application is now visible on screen." );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "The application is now responding to user input." );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "The application no longer responds to user input." );
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "The application is no longer visible." );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "Any memory used by the application is freed." );
+    }
+
 }
