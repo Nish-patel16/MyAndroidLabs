057a581f849d2b82a708d3d600f13f17916406ec
Nish-patel16 <pate1088@algonquinlive.com>
2023-07-20 13:34:06 -0400

    Final Completed week 6

--- app/src/main/java/algonquin/cst2335/pate1088/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/pate1088/ChatRoom.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
